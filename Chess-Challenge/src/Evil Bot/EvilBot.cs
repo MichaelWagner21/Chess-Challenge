29fab18256a6f9b06cb79e4ec5349b99a9d66938
Michael Wagner <michaelwagner021@gmail.com>
2023-08-29 21:05:02 -0600

    commit

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -3,52 +3,98 @@ using System;
 
 namespace ChessChallenge.Example
 {
-    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-    // Plays randomly otherwise.
+    
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
         int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    bool myColor;
+    public Move Think(Board board, Timer timer)
+    {
+        
+        myColor = board.IsWhiteToMove;
+        int bestMoveFoundValue = -30000;
+        Move[] allMoves = board.GetLegalMoves();
 
-        public Move Think(Board board, Timer timer)
-        {
-            Move[] allMoves = board.GetLegalMoves();
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+        
+        Move moveToPlay = allMoves[0];
 
-            foreach (Move move in allMoves)
-            {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
-                {
-                    moveToPlay = move;
+
+        foreach (Move move in allMoves){
+
+            board.MakeMove(move);
+
+
+            
+            if (ValueOfThe(board) > bestMoveFoundValue){
+                moveToPlay = move;
+                bestMoveFoundValue = ValueOfThe(board);
+            }
+            
+
+            Move[] allOpponentMoves = board.GetLegalMoves();
+            bool checkmatePreventionFound = true;
+            foreach (Move opponentMove in allOpponentMoves){
+                checkmatePreventionFound = true;
+                board.MakeMove(opponentMove);
+                Move[] allResponses = board.GetLegalMoves();
+                foreach (Move response in allResponses){
+                    board.MakeMove(response);
+                    if (board.IsInCheckmate()){
+                        checkmatePreventionFound = false;
+                    }
+                    board.UndoMove(response);
+                }
+                board.UndoMove(opponentMove);
+                if (checkmatePreventionFound){
                     break;
-                }
-
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
-                {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
-                }
+                }  
+            }
+            
+            if (!checkmatePreventionFound && bestMoveFoundValue<20000){
+                moveToPlay = move;
+                bestMoveFoundValue = 19999;
             }
 
-            return moveToPlay;
+
+            board.UndoMove(move);
+
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
-        {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+        return moveToPlay;
+    }
+    private int ValueOfThe(Board inputBoard){
+        bool isMyTurn = inputBoard.IsWhiteToMove == myColor;
+        if (inputBoard.IsDraw()){
+            return 0;
         }
+        if (inputBoard.IsInCheckmate()){
+            if (!isMyTurn){
+                return 20000;
+            }
+            else {
+                return -20000;
+            }
+        }
+        int returnValue = 0;
+        PieceList[] allPieces = inputBoard.GetAllPieceLists();
+        foreach (PieceList pieces in allPieces){
+            int modifier = 1;
+            if (pieces.IsWhitePieceList != myColor){
+                modifier = -1;
+            }
+            returnValue+= modifier * pieces.Count * pieceValues[(int)pieces.TypeOfPieceInList];
+        }
+        if (inputBoard.IsInCheck()){
+            if (isMyTurn){
+                returnValue-=1;
+            }
+            else {
+                returnValue+=1;
+            }
+        }
+        Square enemyKingSquare = inputBoard.GetKingSquare(!myColor);
+        return returnValue;
+    }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,11 +7,11 @@ public class MyBot : IChessBot
 {
 
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-    bool mySideIsWhite;
+    bool myColor;
     public Move Think(Board board, Timer timer)
     {
         
-        mySideIsWhite = board.IsWhiteToMove;
+        myColor = board.IsWhiteToMove;
         int bestMoveFoundValue = -30000;
         Move[] allMoves = board.GetLegalMoves();
 
@@ -64,11 +64,12 @@ public class MyBot : IChessBot
         return moveToPlay;
     }
     private int ValueOfThe(Board inputBoard){
+        bool isMyTurn = inputBoard.IsWhiteToMove == myColor;
         if (inputBoard.IsDraw()){
             return 0;
         }
         if (inputBoard.IsInCheckmate()){
-            if (inputBoard.IsWhiteToMove != mySideIsWhite){
+            if (!isMyTurn){
                 return 20000;
             }
             else {
@@ -79,11 +80,20 @@ public class MyBot : IChessBot
         PieceList[] allPieces = inputBoard.GetAllPieceLists();
         foreach (PieceList pieces in allPieces){
             int modifier = 1;
-            if (pieces.IsWhitePieceList != mySideIsWhite){
+            if (pieces.IsWhitePieceList != myColor){
                 modifier = -1;
             }
             returnValue+= modifier * pieces.Count * pieceValues[(int)pieces.TypeOfPieceInList];
         }
+        if (inputBoard.IsInCheck()){
+            if (isMyTurn){
+                returnValue-=1;
+            }
+            else {
+                returnValue+=1;
+            }
+        }
+        Square enemyKingSquare = inputBoard.GetKingSquare(!myColor);
         return returnValue;
     }
 }
